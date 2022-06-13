3b27f439b6880436c0572e09e6abd826ad0ea84b
SimaAjiS <comb.yasushi+github@gmail.com>
2022-06-14 07:24:11 +0900

    変数名の修正 col_L, col_R → col_left, col_right

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 sns.set(style='dark', font='Meiryo')
 
 
