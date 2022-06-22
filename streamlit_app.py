8aceb65a8ec8b35932ffd1f5637a62698c5dc3af
SimaAjiS <comb.yasushi+github@gmail.com>
2022-06-23 00:08:48 +0900

    画面サイズ指定

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support.wait import WebDriverWait
 
 sns.set(style='dark', font='Meiryo')
 
